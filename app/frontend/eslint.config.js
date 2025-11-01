4a85f917676643cd0a6db33e878e0e5e5395c035
Tushar-CodeSpace <tushar.codespace@gmail.com>
2025-11-02 01:42:07 +0530

    add login logic

--- app/frontend/eslint.config.js
+++ app/frontend/eslint.config.js
@@ -4,20 +4,44 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir: __dirname
+      }
+    },
+  },
+  {
+    files: ['frontend/**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./frontend/tsconfig.app.json'],
+      },
+    },
+  },
+  {
+    files: ['app/frontend/**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./app/frontend/tsconfig.app.json'],
+      },
     },
   },
 ])
